3b43c64ebc10afef7089e5a915fcb8375caff4c7
Kocsis Kritsof <kocsis.kris@gmail.com>
2017-06-09 18:59:30 +0200

    Connected UI with AppDelegate
    Hooked up all the UI elements to AppDelegate

--- Stock Calculator/ViewController.swift
+++ Stock Calculator/ViewController.swift
@@ -19,7 +19,49 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var Display: UILabel!
+    
+    @IBAction func AC_C(_ sender: Any) {
+    }
+    @IBAction func Sign(_ sender: Any) {
+    }
+    @IBAction func Percent(_ sender: Any) {
+    }
+    @IBAction func Multiplication(_ sender: Any) {
+    }
+    @IBAction func Division(_ sender: Any) {
+    }
+    @IBAction func Addition(_ sender: Any) {
+    }
+    @IBAction func Subtraction(_ sender: Any) {
+    }
+    @IBAction func Dot(_ sender: Any) {
+    }
+    
+    @IBAction func Nine(_ sender: Any) {
+    }
+    @IBAction func Eight(_ sender: Any) {
+    }
+    @IBAction func Seven(_ sender: Any) {
+    }
+    @IBAction func Six(_ sender: Any) {
+    }
+    @IBAction func Five(_ sender: Any) {
+    }
+    @IBAction func Four(_ sender: Any) {
+    }
+    @IBAction func Three(_ sender: Any) {
+    }
+    @IBAction func Two(_ sender: Any) {
+    }
+    @IBAction func One(_ sender: Any) {
+    }
+    @IBAction func Zero(_ sender: Any) {
+    }
 
+    @IBAction func Equal(_ sender: Any) {
+    }
 
 }
 
