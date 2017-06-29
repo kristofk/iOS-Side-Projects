85cceacebd97539267eb497f4517ca2bf3cb0c3a
Kocsis Kritsof <kocsis.kris@gmail.com>
2017-06-29 16:20:29 +0200

    Extensions added to project
    + extension for getting color from location
    + extension for getting rgb from color

---
+++ ID Color/Extensions.swift
@@ -0,0 +1,50 @@
+//
+//  Extensions.swift
+//  ID Color
+//
+//  Created by Kristof Kocsis on 2017. 06. 29..
+//  Copyright © 2017. Kristof Kocsis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    public func getPixelColor(pos: CGPoint) -> UIColor {
+        
+        let pixelData = self.cgImage!.dataProvider!.data
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        
+        let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+        
+        let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+        let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+        let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+        let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+    
+    
+}
+
+extension UIColor {
+    
+    public func rgb() -> (red:Int, green:Int, blue:Int, alpha:Int)? {
+        var fRed : CGFloat = 0
+        var fGreen : CGFloat = 0
+        var fBlue : CGFloat = 0
+        var fAlpha: CGFloat = 0
+        if self.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha) {
+            let iRed = Int(fRed * 255.0)
+            let iGreen = Int(fGreen * 255.0)
+            let iBlue = Int(fBlue * 255.0)
+            let iAlpha = Int(fAlpha * 255.0)
+            
+            return (red:iRed, green:iGreen, blue:iBlue, alpha:iAlpha)
+        } else {
+            // Could not extract RGBA components:
+            return nil
+        }
+    }
+}
