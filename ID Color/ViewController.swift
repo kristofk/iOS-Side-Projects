9d7a06d8c53b22fda3fc6377799e134cd63c132f
Kocsis Kritsof <kocsis.kris@gmail.com>
2017-06-29 12:54:32 +0200

    Added captureButton
    AddedCaptureButton programatically, but the image assetts are missing
    for now

--- ID Color/ViewController.swift
+++ ID Color/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: SwiftyCamViewController, SwiftyCamViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let buttonFrame = CGRect(x: view.frame.midX - 37.5, y: view.frame.height - 100.0, width: 75.0, height: 75.0)
+        let captureButton = SwiftyCamButton(frame: buttonFrame)
+        self.view.addSubview(captureButton)
+        captureButton.setImage(focus, for: .normal)
+        cameraDelegate = self
+        captureButton.delegate = self
+        view.addSubview(captureButton)
     }
 
     override func didReceiveMemoryWarning() {
